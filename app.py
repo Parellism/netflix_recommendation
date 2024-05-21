4eebc45a9bd549502aebbcd05aa0841acc8977c1
Parellism <136243661+Parellism@users.noreply.github.com>
2024-05-21 16:51:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ movies_list = movies['Title'].values
 st.markdown(page_bg_img, unsafe_allow_html=True)
 st.header(":red[NETFLIX] Movies Recommender System")
 
-select_movie = st.text_input("Select movie", sorted(movies_list))
+select_movie = st.selectbox("Select movie", sorted(movies_list))
 
 def recommend(movie):
     film_req = movies[movies['Title']==movie].index[0]
