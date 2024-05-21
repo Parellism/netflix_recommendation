257c96c13a6b68d00df2540e59cf7e6059be7de8
Parellism <136243661+Parellism@users.noreply.github.com>
2024-05-21 16:44:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -28,9 +28,7 @@ st.header(":red[NETFLIX] Movies Recommender System")
 select_movie = st.text_input("Search")
 #st.selectbox("Select movie", sorted(movies_list))
 
-
-
-if input_text:
+if select_movie:
     matches = [keyword for keyword in movies_list if select_movie.lower() in keyword.lower()]
     st.write("Suggestions:")
     for match in matches:
