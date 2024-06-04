53323c45369766f61d310b2fe043a90ba16851ad
Parellism <136243661+Parellism@users.noreply.github.com>
2024-06-04 19:36:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ movies_list = movies['Title'].values
 st.markdown(page_bg_img, unsafe_allow_html=True)
 st.header(":red[NETFLIX] Movies Recommender System")
 
-select_movie = st.selectbox("Select movie", sorted(movies_list))
+select_movie = st.selectbox("Select movie", movies_list)
 
 def recommend(movie):
     film_req = movies[movies['Title']==movie].index[0]
@@ -36,33 +36,20 @@ def recommend(movie):
         similarity_score = i[1]
         film_index = i[0]
         if similarity_score > 0:
-            #movie_recommendation.append(movies.iloc[film_index].Title)
-            movie_title = movies.iloc[film_index].Title
-            poster_url = movies.iloc[film_index].Poster_URL
-            movie_recommendation.append((movie_title, poster_url))
+            movie_recommendation.append(movies.iloc[film_index].Title)
     return movie_recommendation
 
 if st.button("Show Recommendation"):
-    movie_names_urls = recommend(select_movie)[:5]  # Ambil lima rekomendasi pertama
-    col1, col2, col3, col4, col5 = st.columns(5)
-    for i, (movie_name, poster_url) in enumerate(movie_names_urls):
-        if i == 0:
-            with col1:
-                st.text(movie_name)
-                st.image(poster_url)
-        elif i == 1:
-            with col2:
-                st.text(movie_name)
-                st.image(poster_url)
-        elif i == 2:
-            with col3:
-                st.text(movie_name)
-                st.image(poster_url)
-        elif i == 3:
-            with col4:
-                st.text(movie_name)
-                st.image(poster_url)
-        elif i == 4:
-            with col5:
-                st.text(movie_name)
-                st.image(poster_url)
+    movie_name = recommend(select_movie)
+    col1,col2,col3,col4,col5 = st.columns(5)
+    with col1:
+        st.text(movie_name[0])
+    with col2:
+        st.text(movie_name[1])
+        st.image(f"poster/{movie['Title']}.jpg", use_column_width=True)
+    with col3:
+        st.text(movie_name[2])
+    with col4:
+        st.text(movie_name[3])
+    with col5:
+        st.text(movie_name[4])
